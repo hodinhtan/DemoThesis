25f62d0db91398f67e95940f1ce9e59da42cfc24
Tan Ho <tan.ho@smartcube.vn>
2020-09-14 18:26:19 +0700

    update simulator

--- src/simulator.py
+++ src/simulator.py
@@ -5,7 +5,6 @@
 @organization : hust.edu.vn
 @version : v1.0
 @description : Sensors simulation.
-
 """
 
 import argparse
@@ -14,133 +13,65 @@ import json
 import time
 import random
 from datetime import datetime
-simulate_slots = 12
-DEBUG_MODE = False
-choice = ["normal", "high temp", "fire"]
+from utils.constants import *
 
+class Simulator():
+    def __init__(self, tb_name, json_file):
+        self.gw_telemetry = 'v1/gateway/telemetry'
+        self.tb_name = tb_name
+        self.json_data = self.parseFile(json_file)
+        self.payload = self.createData(json_data)
 
-THINGSBOARD_HOST = ''
-MQTT_PORT = 1883
-ACCESS_TOKEN = ''
-Topic = 'v1/gateway/telemetry'
-INTERVAL = 60
-next_reading = time.time()
-payload = {}
+    def tbInstantce(self, token):
+        try:
+            client = mqtt.Client()
+            client.connected_flag = False
+            client.username_pw_set(token)
+            client.connect(THINGSBOARD_HOST, MQTT_PORT, 60)
+            client.loop_start()
+            return client
+        except Exception as e:
+            print('[ERROR] : {}' .format(e))
 
+        
+    def createData(self, json_data):
+        payload = {}
+        for v in json_data.values():
+            if v in DEVICES:
+               x = json_data[v]
+               for y in x:
+                   payload[y['label']] = random.choice([ 10, 20, 24, 26, 28 ,30 ,40])
+        return payload
+        
+    def parseFile(self, fd):
+        with open(fd, 'rt') as file:
+            data = json.load(file)
+            if(data): 
+                return data
+            else:
+                return 0
 
-def simulateSlotValue():
-	return random.choice(choice)
+    def sendTelemetry(self, client, topic, payload):
+        ret = client.publish(topic, json.dumps(payload), 1)
+        if (ret[0] == 0):
+            print('{} Telemetry sent ...[OK]' .format(datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
+            time.sleep(0.5)
+            if (DEBUG_MODE):
+                print(payload)
+        else:
+            raise ValueError('Telemetry Send')
 
-
-def populateValues():
-	parking_lot = {}
-	for x in range(simulate_slots):
-		name = 'tang1BaoChay' + f'{x+1:02d}'
-		parking_lot[name] = simulateSlotValue()
-	return parking_lot 
-
-
-def msgAssembler(obj):
-	payload = {}
-	for key, value in obj.items():
-		payload[key] = []
-		msg = {
-			'ts' : 0,
-			'values' : {}
-		}
-		msg['ts'] = round(time.time() * 1000)
-		msg['values']['status'] = value
-		payload[key].append(msg)
-	return payload
-
-def on_connect(client, userdata, flags, rc):    
-    if(rc == 0):
-    	print('[INFO] Connected succesfully')
-    	client.connected_flag = True
-    else:
-    	print('[ERROR] Connection Failed')
-    	raise ValueError('Connection Problem')
-
-def on_disconnect(client, userdata, rc):
-	print('[INFO] Disconnection : {}' .format(rc))
-
-
-def connectThingsboard():
-	try:
-		client = mqtt.Client('tanho')
-		client.connected_flag = False
-		client.on_connect = on_connect
-		client.username_pw_set(ACCESS_TOKEN)
-		client.connect(THINGSBOARD_HOST, MQTT_PORT, 60)
-		client.loop_start()
-		return client
-	except Exception as e:
-		print('[ERROR] : {}' .format(e))
-
-	
-def parseFile(fd):
-	with open(fd, 'rt') as file:
-		data = json.load(file)
-		if(data): 
-			return data
-		else:
-			return 0
-
-def sendTelemetry(client, topic, payload):
-	ret = client.publish(topic, json.dumps(payload), 1)
-	if (ret[0] == 0):
-		print('{} Telemetry sent ...[OK]' .format(datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
-		time.sleep(0.5)
-		if (DEBUG_MODE):
-			print(payload)
-	else:
-		raise ValueError('Telemetry Send')
-
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Parking Slot Simulator", epilog="Platform Host IP & Access Token are mandatory fields")
-	parser.add_argument("--file", help="Telemetry file in JSON")
-	parser.add_argument("--host", required=True, help="Thingsboard Server IP")
-	parser.add_argument("--token", required=True, help="Thingsboard Access Token")
-	parser.add_argument("--interval", help="Telemetry interval in seconds")
-	parser.add_argument("--debug", default=False, help="Debug Level True / False")
-	args = parser.parse_args()
-	print("====================================================\n\tSmart Parking Pilot Setup Slot Simulator\n====================================================")
-
-	if(args.host):
-		THINGSBOARD_HOST = args.host
-	if(args.token):
-		ACCESS_TOKEN = args.token
-	if(args.debug):
-		if((args.debug).lower() == 'true'):
-			DEBUG_MODE = True
-			print('[INFO] Debug Mode ...[OK]')
-		elif ((args.debug).lower() == 'false'):
-			DEBUG_MODE = False
-	if(args.interval):
-		if int(args.interval) and int(args.interval) > 10:
-			INTERVAL = int(args.interval)
-			print('[INFO] Time Interval : {} seconds ...[OK]' .format(args.interval))
-
-	client = connectThingsboard()
-	if(args.file):
-		ret = parseFile(args.file)
-		ret = msgAssembler(ret)
-	else:
-		ret = populateValues()
-		ret = msgAssembler(ret)
-	while not client.connected_flag:
-		print('[INFO] Connecting ...')
-		time.sleep(1)
-	try:
-		while True:
-			ret = populateValues()
-			ret = msgAssembler(ret)
-			sendTelemetry(client, Topic, ret)
-			next_reading += INTERVAL
-			sleep_time = next_reading - time.time()
-			if sleep_time > 0:
-				time.sleep(sleep_time) 
-	except KeyboardInterrupt as e:
-		print('[INFO] program exiting ...')
-		client.loop_stop()
-		client.disconnect()
+    def run(self, token):
+        client = self.tbInstantce(token)
+        print(self.payload)
+        while not client.connected_flag:
+            print('[INFO] Connecting ...')
+            time.sleep(1)
+        try:
+            while True:
+                self.sendTelemetry(self, client, self.gw_telemetry, self.payload)
+                time.sleep(INTERVAL_TIME)
+        except KeyboardInterrupt as e:
+            print('[INFO] program exiting ...')
+            client.loop_stop()
+            client.disconnect()
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,10 +1,6 @@
-TOKEN_TANG_1 = "MnAlsMLhEh2AsIwPo54a"
-TOKEN_TANG_2 = "W0j3urLbjkvpsueFcEWB"
-TOKEN_TANG_3 = "s86v2If8Spb2EBRnJxHf"
-TOKEN_TANG_4 = "Lby4z5lBnkRusiMy43UA"
-TOKEN_TANG_5 = "9kErjJCYkfeY4cLCKaMN"
-
-INTERVAL_TIME = 10 #Sec
+# ================================
+# SYSTEM
+DEBUG_MODE = 0
 
 ASSETS_PATH = "/home/tanho/hoy/thesis/assets/"
 IMAGE_PATH = "/home/tanho/hoy/thesis/assets/img/"
@@ -15,3 +11,21 @@ CFG_PATH = "/home/tanho/hoy/thesis/cfg/"
 LABELME_PATH = "/home/tanho/hoy/thesis/cfg/labelme/"
 WIDGET_PATH = "/home/tanho/hoy/thesis/cfg/widget/"
 DEVICE_PATH = "/home/tanho/hoy/thesis/cfg/devices/"
+
+DEVICES = ['BaoChay', 'BaoKhoi', 'Phunnuoc']
+
+# =================================
+# THINGSBOARD
+INTERVAL_TIME = 5 #Sec
+
+TOKEN_TANG_1 = "MnAlsMLhEh2AsIwPo54a"
+TOKEN_TANG_2 = "W0j3urLbjkvpsueFcEWB"
+TOKEN_TANG_3 = "s86v2If8Spb2EBRnJxHf"
+TOKEN_TANG_4 = "Lby4z5lBnkRusiMy43UA"
+TOKEN_TANG_5 = "9kErjJCYkfeY4cLCKaMN"
+
+THINGSBOARD_HOST = 'localhost'
+MQTT_PORT = 1883
+
+
+
